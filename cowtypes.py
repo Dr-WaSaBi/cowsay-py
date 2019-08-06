4679f88c78e3a4e2f8c44560a7f31a51788275eb
Dr-WaSaBi <russellriker@gmail.com>
2019-08-06 16:05:02 -0400

    Added cowtype file
    The definitions for different cows types will be placed in this file.

---
+++ cowtypes.py
@@ -0,0 +1,22 @@
+# cowtype module.
+# The differnt copy types that cowday can print will be definded here
+# Aug, 2019 
+# Russell R. Riker
+
+def build_oddcow():
+    return """
+         \   <__> 
+          \  (db)\_______
+             (__)\       )\/\\
+                 ||----W |
+                 ||     ||
+    """
+
+def build_cow():
+    return """
+         \   ^__^ 
+          \  (oo)\_______
+             (__)\       )\/\\
+                 ||----w |
+                 ||     ||
+    """
